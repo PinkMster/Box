6d9a92d202b9d768d880175ee3608867399fbd3b
lee <softcola6666@gmail.com>
2025-01-29 04:48:41 +0900

    2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
+  base: '/box/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
